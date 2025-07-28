5cb759f65a25a0f93e9b7cdd0da45141ece3ce63
Jose Bautista <jlbautistavelasquez@gmail.com>
2025-07-27 20:07:42 -0500

    Proyecto base

---
+++ src/main/java/com/jlbautistavelasquez/HotelManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jlbautistavelasquez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlbautistavelasquez/HotelManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlbautistavelasquez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
